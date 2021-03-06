5703835d44720a21dab348010709e043bec5dd82
0xHandy <elexa.tomas@zoho.com>
2021-03-06 19:39:44 +0100

    IC

--- connect.js
+++ connect.js
@@ -1,3 +1,17 @@
 const fs = require('fs')
+var QRCode = require('qrcode')
+
+let rawconfig = fs.readFileSync('config.json')
+let config = JSON.parse(rawconfig)
 let serial = fs.readFileSync('serial.txt')
+let canvas = document.getElementById('canvasqr')
+
+if (config.firstime == true) {
+  
+}
+ 
+QRCode.toCanvas(canvas, 'sample text', function (error) {
+  if (error) console.error(error)
+  console.log('success!');
+})
 
